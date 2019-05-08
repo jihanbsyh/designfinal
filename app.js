a13ed413a5790348f3089b4224a644a0a005f142
jihanbsyh <46944721+jihanbsyh@users.noreply.github.com>
2019-05-07 23:57:02 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.parallax');
   var instances = M.Parallax.init(elems);
 });
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, {fullWidth: false});
+});
